684402f4c4bb07a6d287db561a8d018cdf85f27a
simonbeirouti <hello@simonbeirouti.com>
2025-03-10 03:43:14 +1100

    fix: meta issue, add og image

--- app/lib/meta.ts
+++ app/lib/meta.ts
@@ -19,11 +19,11 @@ export const createMeta = ({
 		{property: "og:title", content: title},
 		{property: "og:description", content: description},
 		{property: "og:type", content: type},
-		{property: "og:image", content: image ? `${process.env.SITE_URL}${image}` : process.env.SITE_URL},
-		{property: "og:url", content: url ? `${process.env.SITE_URL}${url}` : process.env.SITE_URL},
+		{property: "og:image", content: "https://webpres.au/content/og.jpg"},
+		{property: "og:url", content: "https://webpres.au/content/og.jpg"},
 		{name: "twitter:card", content: "summary_large_image"},
 		{name: "twitter:title", content: title},
 		{name: "twitter:description", content: description},
-		{name: "twitter:image", content: image ? `${process.env.SITE_URL}${image}` : process.env.SITE_URL},
+		{name: "twitter:image", content: "https://webpres.au/content/og.jpg"},
 	].filter((meta): meta is NonNullable<typeof meta> => Boolean(meta));
 };
