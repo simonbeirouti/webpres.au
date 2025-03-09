fdec27045b825c06ee937e75d0bfa30f0918753b
simonbeirouti <hello@simonbeirouti.com>
2025-03-10 03:25:42 +1100

    feat: setup sitemap generator

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,27 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import VitePluginSitemap from 'vite-plugin-sitemap';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
+  
   return {
-    plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+    plugins: [
+      tailwindcss(), 
+      reactRouter(), 
+      tsconfigPaths(),
+      VitePluginSitemap({
+        hostname: 'https://webpres.au',
+        exclude: ['/api/*', '/sitemap.xml'],
+        outDir: 'build/client',
+        changefreq: 'weekly',
+        priority: 0.7,
+        lastmod: new Date()
+      })
+    ],
     define: {
       'process.env': env
-    }
+    },
   }
 });
